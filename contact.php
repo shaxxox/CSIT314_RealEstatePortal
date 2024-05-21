5800857d961d0c79e4aded622a3c8b38d3e8f669
laurynllcn <160407713+laurynllcn@users.noreply.github.com>
2024-05-21 16:35:40 +0800

    Edit the "contact" page
    added the descriptions for the page

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ if(isset($_POST['send'])){
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $email = filter_var($email, FILTER_SANITIZE_EMAIL);
    $number = $_POST['number'];
    $number = filter_var($number, FILTER_SANITIZE_STRING);
    $message = $_POST['message'];
@@ -63,12 +63,12 @@ if(isset($_POST['send'])){
          <img src="images/contact-img.svg" alt="">
       </div>
       <form action="" method="post">
-         <h3>get in touch</h3>
-         <input type="text" name="name" required maxlength="50" placeholder="enter your name" class="box">
-         <input type="email" name="email" required maxlength="50" placeholder="enter your email" class="box">
-         <input type="number" name="number" required maxlength="10" max="9999999999" min="0" placeholder="enter your number" class="box">
-         <textarea name="message" placeholder="enter your message" required maxlength="1000" cols="30" rows="10" class="box"></textarea>
-         <input type="submit" value="send message" name="send" class="btn">
+         <h3>Get in Touch</h3>
+         <input type="text" name="name" required maxlength="50" placeholder="Enter your name" class="box">
+         <input type="email" name="email" required maxlength="50" placeholder="Enter your email" class="box">
+         <input type="number" name="number" required maxlength="10" max="9999999999" min="0" placeholder="Enter your number" class="box">
+         <textarea name="message" placeholder="Enter your message" required maxlength="1000" cols="30" rows="10" class="box"></textarea>
+         <input type="submit" value="Send Message" name="send" class="btn">
       </form>
    </div>
 
@@ -85,33 +85,33 @@ if(isset($_POST['send'])){
    <div class="box-container">
 
       <div class="box active">
-         <h3><span>how to cancel booking?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>How to cancel booking?</span><i class="fas fa-angle-down"></i></h3>
+         <p>You can cancel a booking from your account in the 'My Bookings' section.</p>
       </div>
 
       <div class="box active">
-         <h3><span>when will I get the possession?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>When will I get the possession?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Possession is given after all payments and documentation are completed.</p>
       </div>
 
       <div class="box">
-         <h3><span>where can I pay the rent?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>Where can I pay the rent?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Pay rent through our secure online portal in the 'Payments' section.</p>
       </div>
 
       <div class="box">
-         <h3><span>how to contact with the buyers?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>How to contact with the buyers?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Use the messaging feature in your dashboard to contact buyers directly.</p>
       </div>
 
       <div class="box">
-         <h3><span>why my listing not showing up?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>Why my listing not showing up?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Ensure your listing meets our guidelines and is approved. Contact support if issues persist.</p>
       </div>
 
       <div class="box">
-         <h3><span>how to promote my listing?</span><i class="fas fa-angle-down"></i></h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+         <h3><span>How to promote my listing?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Promote your listing using our premium services available in the 'Promotions' section.</p>
       </div>
 
    </div>
@@ -119,15 +119,6 @@ if(isset($_POST['send'])){
 </section>
 
 <!-- faq section ends -->
-
-
-
-
-
-
-
-
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
